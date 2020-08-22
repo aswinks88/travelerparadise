93cba6c0877282aba731c1c58ce0fb1039648f19
aswinks88 <aswinks88@gmail.com>
2020-08-22 16:13:47 +1200

    added popular destination, hiking and activities content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,118 +1,63 @@
 import React from "react";
-
+import bg from "../img/queenstown.jpg";
+import well from "../img/wellington.jpg";
 export default function Home() {
   return (
     <div className="main">
-      <section className="main-bg">
-        <div className="main-content">
-          <div className="main-title">
-            <h1>Discover your next destination</h1>
-          </div>
-          <div className="intro">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-              vero repellat voluptate quo hic? Ducimus beatae, omnis vel
-              deleniti aut, saepe quasi voluptas repellat dolore provident
-              reprehenderit natus esse odit?m
-            </p>
-          </div>
+      <section className="welcome">
+        <div className="welcome-middle">
+          <h1>Discover your next adventure</h1>
         </div>
         <div className="searchbox">
-          <div className="search-title">
-            <h3>Find your next destination</h3>
+          <input
+            className="search"
+            type="text"
+            placeholder="eg: queenstown, tongariro etc..."
+          />
+        </div>
+        <div className="btn-search">
+          <a href="#search">Search</a>
+        </div>
+      </section>
+      <section className="populardestination">
+        <h1>Popular Destination</h1>
+        <div className="box">
+          <div className="box1">Cathedral Cove</div>
+          <div className="box2">Wellington cable car</div>
+          <div className="box3">Mt Cook</div>
+          <div className="box4">Milford</div>
+        </div>
+      </section>
+      <section className="recommended">
+        <h1>For Hikers</h1>
+        <div className="hikers">
+          <div className="destinationimg1">image1</div>
+          <div className="destinationimg2">image2</div>
+          <div className="destinationimg3">image3</div>
+          <div className="destinationimg4">image4</div>
+          <div className="destinationimg5">image5</div>
+          <div className="destinationimg6">image6</div>
+          <div className="destinationimg7">image7</div>
+          <div className="destinationimg8">image8</div>
+        </div>
+      </section>
+      <section className="activities">
+        <h1>Fun Activities To Do</h1>
+
+        <div className="funactivities">
+          <div className="desc">
+            <p>
+              New Zealand's South and North Island is home to lot of
+              attractions. A country where you can dig your own hot water pool,
+              surf, ski and hike in the same day. The North Island is a volcanic
+              wonderland with active volcanoes, amazing beaches and lots of
+              wines. On the other hand, South Island is a land with glaciers,
+              lakes with beautiful backdrops and Aoraki mountains, New Zealand
+              highest mountain. Over all, its a place that never disappoints
+              enyone who seek an adeventure.
+            </p>
           </div>
-          <div className="search-field">
-            {/* <label>Destination :</label> */}
-            <input className="search" type="text" />
-          </div>
-          <a href="" className="btn btn-search">
-            Explore
-          </a>
-        </div>
-        <div className="links">
-          {/* <a href="" className="btn btn-outline">
-            explore
-          </a> */}
-        </div>
-      </section>
-      <section className="destination">
-        {/* <div className="destinationlist"> */}
-        <div className="listheading">
-          <h1>Top destinations in New Zealand</h1>
-        </div>
-        <div className="destinationcard">
-          <div className="placecard">list1</div>
-          <div className="placecard">list2</div>
-          <div className="placecard">list3</div>
-          <div className="placecard">list4</div>
-        </div>
-        {/* </div> */}
-      </section>
-      <section className="recentlysearch">
-        <div className="listheading">
-          <h1>Recent Searches</h1>
-          <p>See what people are searching for their next adventure</p>
-        </div>
-        <div className="recentcard">
-          <div className="recentcardlist">list1</div>
-          <div className="recentcardlist">list1</div>
-          <div className="recentcardlist">list1</div>
-          <div className="recentcardlist">list1</div>
-          <div className="recentcardlist">list1</div>
-          <div className="recentcardlist">list1</div>
-        </div>
-      </section>
-      <section className="recentcomments">
-        <div className="commentheading">
-          <h1>Recent Comments</h1>
-          <p>Read what people are talking about the places they have visited</p>
-        </div>
-        <div className="commentsection">
-          <div className="placeimage">image1</div>
-          <div className="commentlist">
-            <div className="comments">
-              <div className="profileimg"></div>
-              <div className="info">
-                <h5>
-                  {" "}
-                  It was an amazing experience It was an amazing experienceIt
-                  was an amazing experience
-                </h5>
-                <p>Aswin, Feb 25, 2020</p>
-              </div>
-            </div>
-            <div className="comments">
-              <div className="profileimg"></div>
-              <div className="info">
-                <h5>
-                  {" "}
-                  It was an amazing experience It was an amazing experienceIt
-                  was an amazing experience
-                </h5>
-                <p>Aswin, Feb 25, 2020</p>
-              </div>
-            </div>
-            <div className="comments">
-              <div className="profileimg"></div>
-              <div className="info">
-                <h5>
-                  It was an amazing experience It was an amazing experienceIt
-                  was an amazing experience
-                </h5>
-                <p>Aswin, Feb 25, 2020</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="recommendation">
-        <div className="commentheading">
-          <h1>Recommended places to visit</h1>
-        </div>
-        <div className="recommendplaces">
-          <div className="recommendlist">list1</div>
-          <div className="recommendlist">list1</div>
-          <div className="recommendlist">list1</div>
+          <div className="activitiesimg1"></div>
         </div>
       </section>
     </div>
