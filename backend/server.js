9c8361d07cc486d261f5b7c968ee253c2796643a
aswinks88 <aswinks88@gmail.com>
2020-09-07 00:14:26 +1200

    change server port to 9000

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const route = require("./Routes/test.routes");
 // require("dotenv").config();
 const dotenv = require("dotenv");
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 const app = express();
 app.use(cors());
 app.use(express.json());
